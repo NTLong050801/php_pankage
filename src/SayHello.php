f87824c07b39d3e231914b634a9d1468f949f82b
ntlong050801 <loglog050801@gmail.com>
2022-08-09 16:33:57 +0700

    update

---
+++ src/SayHello.php
@@ -0,0 +1,12 @@
+<?php
+namespace longg_php\SayHello;
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+    public static function hi(){
+        return "Hii";
+    }
+}
